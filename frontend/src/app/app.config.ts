2441dcecbb84f3ccee80888e65b30e93c0039a61
ramanavenky22 <ramanavenky22@gmail.com>
2025-04-20 18:34:57 -0700

    added login functionality through auth0

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,8 +1,18 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAuth0 } from '@auth0/auth0-angular';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-hxq2jg08pca7x1u4.us.auth0.com',
+      clientId: 'EQrggKzdvxoJLY2e6H3rtdPWa5aUOQd9',
+      authorizationParams: {
+        redirect_uri: window.location.origin,
+        audience: 'https://hr-portal/api' // optional if using RBAC-secured API
+      }
+    })
+  ]
 };
